bce4015567c10e968c1aeee5c339d6c39232c379
basak.kocakaya <basak.kocakaya12@gmail.com>
2024-12-10 14:25:48 +0300

    add deploy information

--- src/modules/criteriaWeight/criteriaWeight.service.ts
+++ src/modules/criteriaWeight/criteriaWeight.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { CriteriaWeight } from './criteriaweight.entity';
+import { CriteriaWeight } from './criteriaWeight.entity';
 
 @Injectable()
 export class CriteriaWeightService {
